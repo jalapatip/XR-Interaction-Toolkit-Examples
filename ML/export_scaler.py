bfa11fbd2266e3626924c8a97b83e26d59b23abd
AdityanJo <adityan@reorder.sg>
2021-02-11 21:34:47 -0800

    changed json format for scaler params

--- ML/export_scaler.py
+++ ML/export_scaler.py
@@ -10,7 +10,48 @@ if __name__=='__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--csv_file', default=None)
     args = parser.parse_args()
-
+    headers = [
+        'headPosx', 
+        'headPosy', 
+        'headPosz', 
+        'headRotx', 
+        'headRoty', 
+        'headRotz', 
+        'headRotQx', 
+        'headRotQy', 
+        'headRotQz', 
+        'headRotQw', 
+        'handRPosx', 
+        'handRPosy', 
+        'handRPosz', 
+        'handRRotx', 
+        'handRRoty', 
+        'handRRotz', 
+        'handRRotQx', 
+        'handRRotQy', 
+        'handRRotQz', 
+        'handRRotQw',
+        'handLPosx', 
+        'handLPosy', 
+        'handLPosz', 
+        'handLRotx', 
+        'handLRoty', 
+        'handLRotz', 
+        'handLRotQx', 
+        'handLRotQy', 
+        'handLRotQz', 
+        'handLRotQw',
+        'tracker1Posx', 
+        'tracker1Posy', 
+        'tracker1Posz', 
+        'tracker1Rotx', 
+        'tracker1Roty', 
+        'tracker1Rotz', 
+        'tracker1RotQx', 
+        'tracker1RotQy', 
+        'tracker1RotQz', 
+        'tracker1RotQw',
+        ]
     if args.csv_file:
         os.makedirs('tmp/', exist_ok=True)
         copy(args.csv_file, 'tmp/.')
@@ -19,12 +60,14 @@ if __name__=='__main__':
     else:
         dataset = CSVDataset(root_path=Config['dataset_path'])
     with open(os.path.join('scaler.json'), 'w') as f:
-        scaler = {
-            'min_':dataset.scaler.min_.tolist(),
-            'scale_':dataset.scaler.scale_.tolist(),
-            'data_min_':dataset.scaler.data_min_.tolist(),
-            'data_max_': dataset.scaler.data_max_.tolist(),
-            'data_range_': dataset.scaler.data_range_.tolist(),
-            'n_samples_seen': dataset.scaler.n_samples_seen_
-        }
+        scaler = {}
+        for idx, header in enumerate(headers):
+            scaler[header]={
+                'min_': dataset.scaler.min_.tolist()[idx],
+                'scale_':dataset.scaler.scale_.tolist()[idx],
+                'data_min_':dataset.scaler.data_min_.tolist()[idx],
+                'data_max_': dataset.scaler.data_max_.tolist()[idx],
+                'data_range_': dataset.scaler.data_range_.tolist()[idx],
+                'n_samples_seen': dataset.scaler.n_samples_seen_
+            }
         json.dump(scaler, f)
