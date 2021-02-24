25e0bf90c81c61327b1ce9a039cd11e257df6815
sloan-swieso-usc <swieso@usc.edu>
2021-02-23 21:09:45 -0800

    Adding gesture experiment data collection code

---
+++ VR/Assets/XROSUI/Scripts/Experiments/DataCollection_ExpGestures.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public enum Gesture {HeadUp, HeadDown, HeadForward, HeadBackward}
+
+public class DataCollection_ExpGestures : DataCollection_ExpBase
+{
+    // To be set in Unity to determine which gesture we are currently collecting data for
+    public Gesture gesture;
+    public XRGrabInteractable grabInteractable;
+    
+    // How often to sample the position for a gesture/movement
+    private static double _timestepSec = 0.1;
+    // How many position samples to include in a gesture/movement
+    public static int samplesPerGesture = 10;
+
+    // Running queue of last _samples_per_gesture positions
+    private Queue<DataContainer_ExpGestures.DataContainer_ExpGesturesPosition> _lastPositions =
+        new Queue<DataContainer_ExpGestures.DataContainer_ExpGesturesPosition>();
+    // Lock to make sure the lastPositions queue is safe to edit or read
+    private readonly object _lastPositionsLock = new object();
+    
+    // List of entire data for the experiment
+    private List<DataContainer_ExpGestures> _dataList = new List<DataContainer_ExpGestures>();
+    
+    private GameObject _head;
+    private GameObject _handR;
+    private GameObject _handL;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        ExpName = "ExpGestures";
+        grabInteractable = GetComponent<XRGrabInteractable>();
+        grabInteractable.onSelectExit.AddListener(EndGesture);
+        ReloadXrDevices();
+    }
+
+    private void ReloadXrDevices()
+    {
+        Dev.Log("Reload Xr Devices");
+        _head = Core.Ins.XRManager.GetXrCamera().gameObject;
+        _handR = Core.Ins.XRManager.GetRightDirectController();
+        _handL = Core.Ins.XRManager.GetLeftDirectController();
+    }
+
+    public override void StartRecording()
+    {
+        base.StartRecording();
+        ReloadXrDevices();
+    }
+
+    public override void StopRecording()
+    {
+        base.StopRecording();
+    }
+
+    public override void LateUpdate()
+    {
+        if (!_isRecording)
+            return;
+
+        lock (_lastPositionsLock)
+        {
+            if (_lastPositions.Count == 0 || Time.deltaTime >= _timestepSec)
+            {
+                var data = new DataContainer_ExpGestures.DataContainer_ExpGesturesPosition
+                {
+                    headPos = _head.transform.localPosition,
+                    headRot = _head.transform.eulerAngles,
+                    headRotQ = _head.transform.rotation,
+                    handRPos = _handR.transform.localPosition,
+                    handRRot = _handR.transform.eulerAngles,
+                    handRRotQ = _handR.transform.rotation,
+                    handLPos = _handL.transform.localPosition,
+                    handLRot = _handL.transform.eulerAngles,
+                    handLRotQ = _handL.transform.rotation
+                };
+                _lastPositions.Enqueue(data);
+                if (_lastPositions.Count > samplesPerGesture)
+                {
+                    _lastPositions.Dequeue();
+                }
+            }
+        }
+    }
+
+    // Update is called once per frame
+    public override void Update()
+    {
+    }
+
+    public override string OutputFileName()
+    {
+        return ExpName + gesture + "_ " + DateTime.Now.ToString("yyyy-MM-dd-hh-mm-ss") + ".csv";
+    }
+
+    public override string OutputData() 
+    {
+        var sb = new StringBuilder();
+        sb.Append(DataContainer_ExpGestures.HeaderToString());
+        foreach (var d in _dataList)
+        {
+            sb.Append(d);
+        }
+        return sb.ToString();
+    }
+
+    public void EndGesture(XRBaseInteractor xrBaseInteractor)
+    {
+        if (!_isRecording)
+            return;
+
+        // Make sure we have control of the lastPositions queue
+        lock (_lastPositionsLock)
+        {
+            // Make sure there are the proper number of position samples to make a gesture
+            if (_lastPositions.Count != samplesPerGesture)
+                return;
+
+            var data = new DataContainer_ExpGestures()
+            {
+                positions = new List<DataContainer_ExpGestures.DataContainer_ExpGesturesPosition>(_lastPositions),
+                gesture = gesture
+            };
+            
+            _dataList.Add(data);
+        }
+    }
+}
---
+++ VR/Assets/XROSUI/Scripts/Experiments/DataContainer_ExpGestures.cs
@@ -0,0 +1,89 @@
+using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+[Serializable]
+public class DataContainer_ExpGestures
+{
+    public struct DataContainer_ExpGesturesPosition
+    {
+        public Vector3 headPos;
+        public Vector3 headRot; //Euler Angles
+        public Quaternion headRotQ; //Quaternion
+        public Vector3 handRPos;
+        public Vector3 handRRot;
+        public Quaternion handRRotQ;
+        public Vector3 handLPos;
+        public Vector3 handLRot;
+        public Quaternion handLRotQ;
+    }
+
+    public List<DataContainer_ExpGesturesPosition> positions;
+    public Gesture gesture;
+    private static string _headerString;
+    
+    public override string ToString()
+    {
+        var toReturn = "";
+        foreach (var position in this.positions)
+        {
+            toReturn += position.headPos.x + "," + position.headPos.y + "," + position.headPos.z + ","
+                        + position.headRot.x + "," + position.headRot.y + "," + position.headRot.z + ","
+                        + position.headRotQ.x + "," + position.headRotQ.y + "," + position.headRotQ.z + "," + position.headRotQ.w + ","
+                        + position.handRPos.x + "," + position.handRPos.y + "," + position.handRPos.z + ","
+                        + position.handRRot.x + "," + position.handRRot.y + "," + position.handRRot.z + ","
+                        + position.handRRotQ.x + "," + position.handRRotQ.y + "," + position.handRRotQ.z + "," + position.handRRotQ.w + ","
+                        + position.handLPos.x + "," + position.handLPos.y + "," + position.handLPos.z + ","
+                        + position.handLRot.x + "," + position.handLRot.y + "," + position.handLRot.z + ","
+                        + position.handLRotQ.x + "," + position.handLRotQ.y + "," + position.handLRotQ.z + "," + position.handLRotQ.w + ",";
+        }
+
+        toReturn += gesture;
+        return toReturn;
+    }
+    
+    public static string HeaderToString()
+    {
+        if (_headerString == null)
+        {
+            _headerString = "";
+            for (int index = 0; index < DataCollection_ExpGestures.samplesPerGesture; index++)
+            {
+                _headerString += nameof(DataContainer_ExpGesturesPosition.headPos) + "x" + index + ","
+                                 + nameof(DataContainer_ExpGesturesPosition.headPos) + "y" + index + ","
+                                 + nameof(DataContainer_ExpGesturesPosition.headPos) + "z" + index + ","
+                                 + nameof(DataContainer_ExpGesturesPosition.headRot) + "x" + index + ","
+                                 + nameof(DataContainer_ExpGesturesPosition.headRot) + "y" + index + ","
+                                 + nameof(DataContainer_ExpGesturesPosition.headRot) + "z" + index + ","
+                                 + nameof(DataContainer_ExpGesturesPosition.headRotQ) + "x" + index + ","
+                                 + nameof(DataContainer_ExpGesturesPosition.headRotQ) + "y" + index + ","
+                                 + nameof(DataContainer_ExpGesturesPosition.headRotQ) + "z" + index + ","
+                                 + nameof(DataContainer_ExpGesturesPosition.headRotQ) + "w" + index + ","
+                                 + nameof(DataContainer_ExpGesturesPosition.handRPos) + "x" + index + ","
+                                 + nameof(DataContainer_ExpGesturesPosition.handRPos) + "y" + index + ","
+                                 + nameof(DataContainer_ExpGesturesPosition.handRPos) + "z" + index + ","
+                                 + nameof(DataContainer_ExpGesturesPosition.handRRot) + "x" + index + ","
+                                 + nameof(DataContainer_ExpGesturesPosition.handRRot) + "y" + index + ","
+                                 + nameof(DataContainer_ExpGesturesPosition.handRRot) + "z" + index + ","
+                                 + nameof(DataContainer_ExpGesturesPosition.handRRotQ) + "x" + index + ","
+                                 + nameof(DataContainer_ExpGesturesPosition.handRRotQ) + "y" + index + ","
+                                 + nameof(DataContainer_ExpGesturesPosition.handRRotQ) + "z" + index + ","
+                                 + nameof(DataContainer_ExpGesturesPosition.handRRotQ) + "w" + index + ","
+                                 + nameof(DataContainer_ExpGesturesPosition.handLPos) + "x" + index + ","
+                                 + nameof(DataContainer_ExpGesturesPosition.handLPos) + "y" + index + ","
+                                 + nameof(DataContainer_ExpGesturesPosition.handLPos) + "z" + index + ","
+                                 + nameof(DataContainer_ExpGesturesPosition.handLRot) + "x" + index + ","
+                                 + nameof(DataContainer_ExpGesturesPosition.handLRot) + "y" + index + ","
+                                 + nameof(DataContainer_ExpGesturesPosition.handLRot) + "z" + index + ","
+                                 + nameof(DataContainer_ExpGesturesPosition.handLRotQ) + "x" + index + ","
+                                 + nameof(DataContainer_ExpGesturesPosition.handLRotQ) + "y" + index + ","
+                                 + nameof(DataContainer_ExpGesturesPosition.handLRotQ) + "z" + index + ","
+                                 + nameof(DataContainer_ExpGesturesPosition.handLRotQ) + "w" + index + ",";
+            }
+
+            _headerString += nameof(gesture);
+        }
+        return _headerString;
+    }
+}
