69bea721713fff8fc94fc4f616504ca4b8c53b90
powenyao <powenyao@gmail.com>
2021-03-08 21:24:29 -0800

    Measurement V2

---
+++ VR/Assets/Switch_SaveCalibration.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class Switch_SaveCalibration : Switch_Base
+{
+    public MeasurementsV2 measurement;
+
+    protected override void OnActivated(XRBaseInteractor obj)
+    {
+        measurement.SaveUserData();
+    }
+}
---
+++ VR/Assets/XROSUI/Scripts/UI/Switch_Base.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+[RequireComponent(typeof(XRGrabInteractable))]
+public class Switch_Base : MonoBehaviour
+{
+    protected XRGrabInteractable _grabInteractable;    
+    private void OnEnable()
+    {
+        if (!_grabInteractable)
+        {
+            _grabInteractable = GetComponent<XRGrabInteractable>();
+        }
+        _grabInteractable.onActivate.AddListener(OnActivated);
+        
+    }
+
+    private void OnDisable()
+    {
+        _grabInteractable.onActivate.RemoveListener(OnActivated);
+    }
+
+    protected virtual void OnActivated(XRBaseInteractor obj)
+    {
+        
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
