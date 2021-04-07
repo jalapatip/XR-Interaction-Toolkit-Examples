34d6cfb23a60c057436aa04bcab175ef96513662
sloan-swieso-usc <swieso@usc.edu>
2021-04-06 22:07:37 -0700

    Typing data collection as TestScene_ViveTypingDataCollection.unity

---
+++ VR/Assets/XROSUI/Scripts/Experiments/DataCollection_ExpTyping.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using Random = System.Random;
+
+public class DataCollection_ExpTyping : DataCollection_ExpBase, IWriteToFile
+{
+    public XRGrabInteractable grabInteractable;
+    
+    // List of entire data for the experiment
+    private List<int> _keyList = new List<int>();
+
+    private bool _startedKeyType = false;
+    private bool _completedKeyType = false;
+    private Random _rand = new Random();
+    private int _targetKey;
+    private int _entriesCount = 0;
+
+    private static string _headerString;
+    
+    
+    // Start is called before the first frame update
+    private void OnEnable()
+    {
+        _targetKey = _rand.Next(0, 10);
+        grabInteractable.onSelectEnter.AddListener(StartGesture);
+        grabInteractable.onSelectExit.AddListener(EndGesture);
+        Controller_XR.EVENT_NewPosition += OnNewPosition;
+    }
+
+    private void OnDisable()
+    {
+        grabInteractable.onSelectEnter.RemoveListener(StartGesture);
+        grabInteractable.onSelectExit.RemoveListener(EndGesture);
+        Controller_XR.EVENT_NewPosition -= OnNewPosition;
+    }
+
+    private void StartGesture(XRBaseInteractor arg0)
+    {
+        _startedKeyType = true;
+    }
+
+    public void EndGesture(XRBaseInteractor xrBaseInteractor)
+    {
+        _completedKeyType = true;
+    }
+
+    public override void LateUpdate()
+    {
+    }
+
+    // Update is called once per frame
+    public override void Update()
+    {
+    }
+
+    public void OnNewPosition()
+    {
+        if (_completedKeyType)
+        {
+            print("Done adding key " + _targetKey);
+            _keyList.Add(_targetKey);
+            _startedKeyType = false;
+            _completedKeyType = false;
+            _targetKey = _rand.Next(0, 10);
+            _entriesCount++;
+        }
+        else if (_startedKeyType)
+        {
+            print("Adding key " + _targetKey);
+            _keyList.Add(_targetKey);
+        }
+        else
+        {
+            _keyList.Add(-1);
+        }
+    }
+
+    public override string OutputFileName()
+    {
+        return "Exp3" + "_"  + DateTime.Now.ToString("yyyy-MM-dd-hh-mm-ss") + ".csv";
+    }
+
+    public override string OutputData()
+    {
+        var positions = Core.Ins.XRManager.GetLastPositionSamples(_keyList.Count);
+        print("Outputting data");
+        var sb = new StringBuilder();
+        sb.Append(HeaderToString());
+        for (int i = 0; i < _keyList.Count; i++)
+        {
+            sb.Append(positions[i]);
+            sb.Append(_keyList[i]);
+        }
+        return sb.ToString();
+    }
+
+    public void RemoveLastGesture()
+    {
+        // todo: In gestureList find the last non-None gesture and set it to None
+    }
+
+    public int GetTotalEntries()
+    {
+        return _entriesCount;
+    }
+
+    public int GetTargetKey()
+    {
+        return _targetKey;
+    }
+    
+    public static string HeaderToString()
+    {
+        if (_headerString == null)
+        {
+            _headerString = nameof(PositionSample.timestamp) + ","
+                             + nameof(PositionSample.headPos) + "x,"
+                             + nameof(PositionSample.headPos) + "y,"
+                             + nameof(PositionSample.headPos) + "z,"
+                             + nameof(PositionSample.headRot) + "x,"
+                             + nameof(PositionSample.headRot) + "y,"
+                             + nameof(PositionSample.headRot) + "z,"
+                             + nameof(PositionSample.headRotQ) + "x,"
+                             + nameof(PositionSample.headRotQ) + "y,"
+                             + nameof(PositionSample.headRotQ) + "z,"
+                             + nameof(PositionSample.headRotQ) + "w,"
+                             + nameof(PositionSample.handRPos) + "x,"
+                             + nameof(PositionSample.handRPos) + "y,"
+                             + nameof(PositionSample.handRPos) + "z,"
+                             + nameof(PositionSample.handRRot) + "x,"
+                             + nameof(PositionSample.handRRot) + "y,"
+                             + nameof(PositionSample.handRRot) + "z,"
+                             + nameof(PositionSample.handRRotQ) + "x,"
+                             + nameof(PositionSample.handRRotQ) + "y,"
+                             + nameof(PositionSample.handRRotQ) + "z,"
+                             + nameof(PositionSample.handRRotQ) + "w,"
+                             + nameof(PositionSample.handLPos) + "x,"
+                             + nameof(PositionSample.handLPos) + "y,"
+                             + nameof(PositionSample.handLPos) + "z,"
+                             + nameof(PositionSample.handLRot) + "x,"
+                             + nameof(PositionSample.handLRot) + "y,"
+                             + nameof(PositionSample.handLRot) + "z,"
+                             + nameof(PositionSample.handLRotQ) + "x,"
+                             + nameof(PositionSample.handLRotQ) + "y,"
+                             + nameof(PositionSample.handLRotQ) + "z,"
+                             + nameof(PositionSample.handLRotQ) + "w,"
+                             + "key";
+        }
+
+        return _headerString;
+    }
+}
---
+++ VR/Assets/XROSUI/Scripts/UI/Label_ShowCurrentTypeGoal.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Label_ShowCurrentTypeGoal : MonoBehaviour
+{
+    private TMP_Text text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = this.GetComponent<TMP_Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DataCollection_ExpTyping exp = (DataCollection_ExpTyping)Core.Ins.DataCollection.currentExperiment;
+        text.text = "Enter Key: " + exp.GetTargetKey() + "\nTotal Entries: " + exp.GetTotalEntries();
+    }
+}
