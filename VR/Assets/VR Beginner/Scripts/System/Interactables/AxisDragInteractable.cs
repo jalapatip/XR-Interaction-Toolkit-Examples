41524490ab316b85c28141ff5c2833ecf731105d
powenyao <powenyao@gmail.com>
2021-06-21 13:51:13 -0700

    Preparation to upgrade XR Plugin Management

--- VR/Assets/VR Beginner/Scripts/System/Interactables/AxisDragInteractable.cs
+++ VR/Assets/VR Beginner/Scripts/System/Interactables/AxisDragInteractable.cs
@@ -140,17 +140,17 @@ public class AxisDragInteractable : XRBaseInteractable
         }
     }
 
-    protected override void OnSelectEnter(XRBaseInteractor interactor)
+    protected override void OnSelectEntered(XRBaseInteractor interactor)
     {
-        base.OnSelectEnter(interactor);
+        base.OnSelectEntered(interactor);
 
         m_GrabbedOffset = interactor.transform.position - transform.position;
         m_GrabbingInteractor = interactor;
     }
 
-    protected override void OnSelectExit(XRBaseInteractor interactor)
+    protected override void OnSelectExited(XRBaseInteractor interactor)
     {
-        base.OnSelectExit(interactor);
+        base.OnSelectExited(interactor);
 
         if (SnapOnlyOnRelease && Steps != 0)
         {
