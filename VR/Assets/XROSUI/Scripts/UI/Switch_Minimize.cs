79b3d9ccfeec9cbba105d25bf40afeb2a95fc4e8
powenyao <powenyao@gmail.com>
2021-06-22 13:33:32 -0700

    Fixxes to Mirror and Minimize Switch

--- VR/Assets/XROSUI/Scripts/UI/Switch_Minimize.cs
+++ VR/Assets/XROSUI/Scripts/UI/Switch_Minimize.cs
@@ -4,19 +4,28 @@ using UnityEngine;
 using UnityEngine.Serialization;
 using UnityEngine.XR.Interaction.Toolkit;
 
+/// <summary>
+/// This switch minimize script work by setting gameobjects inactive.
+/// To use this switch, drag and drop things that should be set inactive in the list when setting up the prefab.
+/// To avoid minimizing itself, the prefab should look like:
+/// GO_XXX <- top level gameobject that's there to store any script that should always be running
+/// GO_XXX > GO_ThingsThatShouldBeMinimized
+/// GO_XXX > Switch_Minimize Prefab
+/// 
+/// </summary>
 [RequireComponent(typeof(XRGrabInteractable))]
 public class Switch_Minimize : MonoBehaviour
 {
     private XRGrabInteractable _grabInteractable;
 
     private bool _isMinimized = false;
-    [FormerlySerializedAs("IsMinimized")] public bool MinimizeAtStart;
+    [FormerlySerializedAs("IsMinimized")] 
+    public bool MinimizeAtStart;
     public List<GameObject> MinimizeList;
 
     private void OnEnable()
     {
         _grabInteractable = GetComponent<XRGrabInteractable>();
-
         _grabInteractable.onActivate.AddListener(OnActivated);
 
         if (MinimizeAtStart)
