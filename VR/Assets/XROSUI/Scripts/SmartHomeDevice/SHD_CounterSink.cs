9314eb60253e415b123f4062d8c575ca94d80fe6
HuanpuHu-USC <78895853+HuanpuHu-USC@users.noreply.github.com>
2021-10-11 21:49:10 +0800

    Add files via upload
    Animation Script for
    SHD_CounterSink, SHD_Washer, SHD_Cabinet, SHD_Oven

--- VR/Assets/XROSUI/Scripts/SmartHomeDevice/SHD_CounterSink.cs
+++ VR/Assets/XROSUI/Scripts/SmartHomeDevice/SHD_CounterSink.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,12 +7,35 @@ public class SHD_CounterSink : SmartHomeDevice
     // Start is called before the first frame update
     void Start()
     {
-        
+        SmartHomeManager shm = (SmartHomeManager)Core.Ins.DataCollection.GetCurrentExperiment();
+        shm.AddTarget("Say 'open' while looking at it", this);
+        shm.AddTarget("Say 'open' while pointing at it", this);
+        shm.AddTarget("Say 'open oven'", this);
     }
 
     // Update is called once per frame
     void Update()
     {
+        DebugUpdate();
+    }
+
+    void DebugUpdate()
+    {
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            this.OpenDevice(true);
+	    print("CounterSink Open!!!");
+        }
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+            this.OpenDevice(false);
+	    print("CounterSink Close!!!");	    
+        }
         
     }
+
+    // public override void OpenDevice(bool b)
+    // {
+    //
+    // }
 }
--- VR/Assets/XROSUI/Scripts/SmartHomeDevice/SHD_DishWasher.cs
+++ VR/Assets/XROSUI/Scripts/SmartHomeDevice/SHD_DishWasher.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,12 +7,43 @@ public class SHD_DishWasher : SmartHomeDevice
     // Start is called before the first frame update
     void Start()
     {
-        
+        SmartHomeManager shm = (SmartHomeManager)Core.Ins.DataCollection.GetCurrentExperiment();
+        shm.AddTarget("Say 'open' while looking at it", this);
+        shm.AddTarget("Say 'open' while pointing at it", this);
+        shm.AddTarget("Say 'open oven'", this);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        DebugUpdate();
     }
+
+    void DebugUpdate()
+    {
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            this.OpenDevice(true);
+	    print("DishWasher Open!!!");
+        }
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+            this.OpenDevice(false);
+	    print("DishWasher Close!!!");	    
+        }
+        
+        if (Input.GetKeyUp(KeyCode.E))
+        {
+            this.TurnOnLight(true);
+        }
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            this.TurnOnLight(false);
+        }
+    }
+
+    // public override void OpenDevice(bool b)
+    // {
+    //
+    // }
 }
