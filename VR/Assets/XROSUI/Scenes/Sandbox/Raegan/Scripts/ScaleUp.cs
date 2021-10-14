cebbca9808d23d76b5423127ed8237046a0e1edd
michael du <msdu@usc.edu>
2021-10-14 14:05:41 -0700

    Fixed Some Prefabs

--- VR/Assets/XROSUI/Scenes/Sandbox/Raegan/Scripts/ScaleUp.cs
+++ VR/Assets/XROSUI/Scenes/Sandbox/Raegan/Scripts/ScaleUp.cs
@@ -1,68 +1,63 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.iOS;
 
 public class ScaleUp : MonoBehaviour
-    
-{
 
+{
     float index = 0;
 
     public bool gameStart;
+
     public void Start()
     {
-        transform.localScale = new Vector3(transform.localScale.x,0, transform.localScale.z);
-        gameStart = StartandStop.InitGame;
-
-        
-
+        transform.localScale = new Vector3(transform.localScale.x, 0, transform.localScale.z);
     }
-   /* float lerpDuration = 3;
+
+    float lerpDuration = 3;
     float startValue = 0;
     float endValue = 2;
     float valueToLerp;
 
-    /*void Start()
-        {
-            StartCoroutine(Lerp());
-        }
 
-        IEnumerator Lerp()
-        {
-            float timeElapsed = 0;
-
-
-            while (timeElapsed < lerpDuration)
-            {
-                valueToLerp = Mathf.Lerp(startValue, endValue, timeElapsed / lerpDuration);
-                timeElapsed += Time.deltaTime;
-                yield return null;
-
-            }
-
-            valueToLerp = endValue;
-        }
-
-
-
-
-
-        */
-    public float scaleSpeed = 1f;
-
-    public void Update()
+    IEnumerator Lerp()
     {
-        if (gameStart)
-        {
-            if (index < 1)
-            {
+        float timeElapsed = 0;
+     
 
-                transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y + 0.5f * Time.deltaTime, transform.localScale.z);
-                index += 0.05f;
-            }
-            gameStart = false;
+        while (timeElapsed < lerpDuration)
+        {
+            valueToLerp = Mathf.Lerp(startValue, endValue, timeElapsed / lerpDuration);
+        
+
+            transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y + valueToLerp * Time.deltaTime, transform.localScale.z);
+
+          
+            timeElapsed += Time.deltaTime;
+            yield return null;
         }
+
+        valueToLerp = endValue;
+        transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y + valueToLerp * Time.deltaTime, transform.localScale.z);
+
+        wallComplete = true;
     }
 
 
+    public float scaleSpeed = 1f;
+    private bool wallComplete = false;
+
+    public void Update()
+    {
+        if (!wallComplete)
+        {
+         
+            StartCoroutine(Lerp());
+        }
+        else
+        {
+           
+        }
+    }
 }
